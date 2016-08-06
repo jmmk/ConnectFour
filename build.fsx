8a67aa8ec83c32e7a757cfa90fb2b562d694f9a8
Michael McLellan <jmikem825@gmail.com>
2016-08-05 21:49:15 -0400

    Use fable in FAKE script

--- build.fsx
+++ build.fsx
@@ -3,36 +3,43 @@
 #r "packages/FSharp.Compiler.Service/lib/net45/FSharp.Compiler.Service.dll"
 
 open Fake
+open Fake.ProcessHelper
 open Fake.Testing
 open Fantomas.FakeHelpers
 open Fantomas.FormatConfig
 open System
 
-let tempDir = "temp"
-let buildDir = tempDir + "/bin"
-let testAssemblies = buildDir + "/*Tests*.dll"
-let solutionFile = "ConnectFour.sln"
-
+let srcDir = "src/ConnectFour"
+let testDir = "tests/ConnectFour.Tests"
 let fantomasConfig = 
     { FormatConfig.Default with PageWidth = 120
                                 ReorderOpenDeclaration = true }
 
-Target "Clean" (fun _ -> CleanDirs [ tempDir ])
+Target "Clean" (fun _ -> CleanDirs ["public"; srcDir + "out"; testDir + "out"])
 Target "Build" (fun _ -> 
-    !!solutionFile
-    |> MSBuildRelease buildDir "Rebuild"
-    |> ignore)
+    execProcess (fun info ->
+        info.FileName <- "npm"
+        info.WorkingDirectory <- srcDir
+        info.Arguments <- "run build") (TimeSpan.FromMinutes 5.0)
+        |> ignore)
+
 Target "Test" (fun _ -> 
-    !!testAssemblies |> NUnit3(fun p -> 
-                            { p with ShadowCopy = true
-                                     TimeOut = TimeSpan.FromMinutes 20.
-                                     Labels = LabelsLevel.All }))
+    execProcess (fun info ->
+        info.FileName <- "npm"
+        info.WorkingDirectory <- testDir
+        info.Arguments <- "run test") (TimeSpan.FromMinutes 5.0)
+        |> ignore)
+
 Target "Format" (fun _ -> 
     !!"build.fsx" ++ "src/**/*.fs" ++ "tests/**/*.fs"
     |> formatCode fantomasConfig
     |> ignore)
+
 Target "Default" DoNothing
-"Clean" ==> "Build"
-"Build" ==> "Default"
-"Build" ==> "Test"
+
+"Clean" 
+    ==> "Build"
+    ==> "Default"
+    ==> "Test"
+
 RunTargetOrDefault "Default"
